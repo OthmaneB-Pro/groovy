ce0d12e3f6f91dd5f69976ce1c90f38412300dc7
OthmaneB-Pro <othmane.baar@gmail.com>
2025-10-20 17:42:58 +0200

    ♻️  TSE-01 > add TextInput.tsx (part 1)

--- src/components/reusable-ui/TextInput.tsx
+++ src/components/reusable-ui/TextInput.tsx
@@ -1,20 +1,42 @@
-import React from "react"
-import styled, { css } from "styled-components"
-import { theme } from "../../theme"
+import React from "react";
+import styled, { css } from "styled-components";
+import { theme } from "../../theme";
 
-const TextInput = React.forwardRef(
-  ({ onChange, Icon, className, version = "normal", ...extraProps }, ref) => {
+type TextInputVersion = "normal" | "minimalist";
+
+type TextInputProps = {
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  Icon?: JSX.Element;
+  className?: string;
+  version?: TextInputVersion;
+};
+
+const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
+  (
+    {
+      onChange,
+      Icon,
+      className,
+      version = "normal",
+      ...extraProps
+    }: TextInputProps,
+    ref
+  ) => {
     return (
       <TextInputStyled className={className} version={version}>
         <div className="icon">{Icon && Icon}</div>
         <input ref={ref} onChange={onChange} type="text" {...extraProps} />
       </TextInputStyled>
-    )
+    );
   }
-)
+);
 
-export default TextInput
-const TextInputStyled = styled.div`
+type TextInputStyledProps = {
+  version: TextInputVersion;
+};
+
+export default TextInput;
+const TextInputStyled = styled.div<TextInputStyledProps>`
   border-radius: ${theme.borderRadius.round};
   display: flex;
   align-items: center;
@@ -36,12 +58,12 @@ const TextInputStyled = styled.div`
   }
 
   /* ${(props) => {
-    if (props.version === "normal") return extraStyleNormal
-    if (props.version === "minimalist") return extraStyleMinimalist
+    if (props.version === "normal") return extraStyleNormal;
+    if (props.version === "minimalist") return extraStyleMinimalist;
   }} */
 
   ${({ version }) => extraStyle[version]}
-`
+`;
 
 const extraStyleNormal = css`
   background-color: ${theme.colors.white};
@@ -55,7 +77,7 @@ const extraStyleNormal = css`
       background: ${theme.colors.white};
     }
   }
-`
+`;
 
 const extraStyleMinimalist = css`
   background-color: ${theme.colors.background_white};
@@ -70,9 +92,9 @@ const extraStyleMinimalist = css`
       outline: 0; //// add outline
     }
   }
-`
+`;
 
 const extraStyle = {
   normal: extraStyleNormal,
   minimalist: extraStyleMinimalist,
-}
+};
