b81cc754afe380d1921d6c5785d7a5472bd5a397
OthmaneB-Pro <othmane.baar@gmail.com>
2025-10-31 22:09:08 +0100

    ♻️  TSE-23-Modal :  add types > initialiseUserSession

--- src/components/pages/order/helpers/initialiseUserSession.tsx
+++ src/components/pages/order/helpers/initialiseUserSession.tsx
@@ -1,4 +1,6 @@
+import { getCategories } from "@/api/categories";
 import { getMenu } from "@/api/product";
+import { Category } from "@/types/Category";
 import { BasketProductQuantity, Product } from "@/types/Product";
 import { getLocalStorage } from "@/utils/window";
 
@@ -14,16 +16,26 @@ const intialiseBasket = (
   username: string,
   setBasket: React.Dispatch<React.SetStateAction<BasketProductQuantity[]>>
 ) => {
-  const basketReceived = getLocalStorage(username); // localStorage est synchrone, pas besoin de "await".
+  const basketReceived = getLocalStorage(username);
   if (basketReceived) setBasket(basketReceived as BasketProductQuantity[]);
+};
+
+const intialiseCategories = async (
+  username: string,
+  setCategories: React.Dispatch<React.SetStateAction<Category[]>>
+) => {
+  const categoriesReceived = await getCategories(username);
+  setCategories(categoriesReceived as Category[]);
 };
 
 export const initialiseUserSession = async (
   username: string,
   setMenu: React.Dispatch<React.SetStateAction<Product[] | undefined>>,
-  setBasket: React.Dispatch<React.SetStateAction<BasketProductQuantity[]>>
+  setBasket: React.Dispatch<React.SetStateAction<BasketProductQuantity[]>>,
+  setCategories: React.Dispatch<React.SetStateAction<Category[]>>
 ): Promise<void> => {
   if (!username) return;
   await intialiseMenu(username, setMenu);
+  await intialiseCategories(username, setCategories);
   intialiseBasket(username, setBasket);
 };
