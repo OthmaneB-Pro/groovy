29794243b23a8e1cb18de70489e4d7bd651d6521
OthmaneB-Pro <othmane.baar@gmail.com>
2025-12-27 12:34:07 +0100

    🐛 fix-price

--- src/components/pages/order/Main/Basket/BasketHeader/helper.ts
+++ src/components/pages/order/Main/Basket/BasketHeader/helper.ts
@@ -1,21 +1,22 @@
+import { Menu } from "@/types/Menu";
 import { BasketProductQuantity, Product } from "@/types/Product";
-import { findObjectById } from "@/utils/array";
 import { convertStringToBoolean } from "@/utils/string";
+import { getBasketItem } from "../BasketBody/helper";
 
 export const calculateSumToPay = (
   basket: BasketProductQuantity[],
-  menu: Product[]
+  menu: Product[] | undefined,
+  menuPack: Menu[] | undefined
 ): number => {
-  return basket.reduce((total, basketProduct) => {
-    if (menu === undefined) return 0;
+  if (menu === undefined || menuPack === undefined) return 0;
+  return basket.reduce((total, basketProductQuantity) => {
+    let itemFound = getBasketItem(basketProductQuantity.id, menu, menuPack);
 
-    const menuProduct = findObjectById(basketProduct.id, menu);
+    if (!itemFound) return total;
+    if (isNaN(itemFound.price)) return total;
 
-    if (!menuProduct) return total;
-    if (isNaN(menuProduct.price)) return total;
-
-    if (convertStringToBoolean(menuProduct.isAvailable) === false) return total;
-    total += menuProduct.price * basketProduct.quantity;
+    if (convertStringToBoolean(itemFound.isAvailable) === false) return total;
+    total += itemFound.price * basketProductQuantity.quantity;
     return total;
   }, 0);
 };
